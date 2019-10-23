4c1f852566c1903d94c99c1ed694c573a358c4cb
Mr.X <790543974@qq.com>
2019-10-23 20:16:06 +0800

    UI模块初步

---
+++ LRRHStory/Assets/XFramework/GameMgr/DataBinding/TestAttributeInstance.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XFramework
+{
+    [XTest("1111", 1)]
+    public class TestAttributeInstance
+    {
+    }
+}
--- LRRHStory/Assets/XFramework/GameMgr/UIMgr/UIMgr.cs
+++ LRRHStory/Assets/XFramework/GameMgr/UIMgr/UIMgr.cs
@@ -2,17 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UIMgr : MonoBehaviour
+namespace XFramework
 {
-    // Start is called before the first frame update
-    void Start()
+    public class UIMgr : GameMgr<UIMgr>
     {
-        
-    }
+        public override void Init()
+        {
+            base.Init();
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public override void Start()
+        {
+            base.Start();
+        }
+
+        public override void EnterGame()
+        {
+            base.EnterGame();
+        }
+
+        public override void Update()
+        {
+            base.Update();
+        }
+
+        public override void Destroy()
+        {
+            base.Destroy();
+        }
+
+        public void OpenUI()
+        {
+
+        }
+
+        public void CloseUI()
+        {
+
+        }
+
+        public void HideUI()
+        {
+
+        }
+
+        public void FindUI()
+        {
+
+        }
     }
 }
+
