829575858d2288a6b0c4475256aa0503a80e4c53
Mr.X <32384543+XuHW1995@users.noreply.github.com>
2019-11-13 23:05:49 +0800

    Dotween插件

---
+++ LRRHStory/Assets/DotweenTest/DotweenTestCode.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class DotweenTestCode : MonoBehaviour
+{
+    public Transform trans;
+
+    public RectTransform Mpanel;
+
+    private Tweener m_Tweener;
+
+    private bool m_isIn = false;
+
+    void Start()
+    {
+        //DOTween.To(() => trans.localPosition, x => trans.localPosition = x, new Vector3(10,10,10), 5);
+        m_Tweener = Mpanel.DOLocalMove(Vector3.zero, 1, true);
+        m_Tweener.SetAutoKill(false);
+        m_Tweener.Pause();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTestClick()
+    {
+        if (!m_isIn)
+        {
+            m_isIn = true;
+            m_Tweener.PlayForward();
+        }
+        else
+        {
+            m_isIn = false;
+            m_Tweener.PlayBackwards();
+        }
+    }
+}
