57c20e9f3cd2d6aab66ea5e34793a57e58470252
Xuhongwei <790543974@qq.com>
2020-05-18 20:46:04 +0800

    分层动画控制器测试模块

--- LRRHStory/Assets/GameLogic/Camera/CameraCtrl.cs
+++ LRRHStory/Assets/GameLogic/Camera/CameraCtrl.cs
@@ -3,7 +3,7 @@
 public class CameraCtrl : MonoBehaviour
 {
     public Transform m_TargetAgent;
-    private Vector3 m_OffsetDis = new Vector3(0,2,-5);
+    private Vector3 m_OffsetDis = new Vector3(0,2,5);
     public void Start()
     {
         //m_OffsetDis = this.transform.position - m_TargetAgent.position; 
